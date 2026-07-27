ea263690157b0748e38fc6756cabd7630774fb85
Jiheon Kim <jihun62113@gmail.com>
2026-07-27 23:31:08 +0900

    test: 커넥션 풀 수정

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '../generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString, max: Number(process.env.DB_POOL_MAX ?? 10) });
 
 export const prisma = new PrismaClient({ adapter });
